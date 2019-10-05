d4c45faee351619cd87ac3bb59dc7a7cdc4f843e
sketchycode <jeremy@sketchyventures.com>
2019-10-04 20:22:21 -0600

    initial commit

---
+++ Assets/Scripts/ShipCore.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ShipCore : MonoBehaviour {
+    private void Start() {
+        var rb2D = GetComponent<Rigidbody2D>();
+        rb2D.AddForceAtPosition(Vector2.one * 5f, Vector2.left);
+    }
+}
