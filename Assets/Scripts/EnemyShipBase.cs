116962a5f0a78a73d776bdb0c1cb24f218c6b801
Chase S <cas6193@psu.edu>
2019-10-05 20:37:04 -0600

    add screen wrap to base ship

---
+++ Assets/Scripts/EnemyShipBase.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EnemyShipBase : MonoBehaviour
+{
+    public float TurnRate = 1   ;
+
+    private Transform _playerShip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Grab player ship upon instantiation
+        _playerShip = FindObjectOfType<ShipCore>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Point towards player ship
+        //Move towards player ship
+        // If collide explode and remove node.
+        FindPlayer();
+    }
+
+    private void FindPlayer()
+    {
+        if (_playerShip != null)
+            transform.up = Vector3.Lerp(transform.up, (_playerShip.position - transform.position), TurnRate);
+        //transform.LookAt(_playerShip, Vector3.back);
+    }
+}
--- Assets/Scripts/ShipCore.cs
+++ Assets/Scripts/ShipCore.cs
@@ -18,6 +18,10 @@ public class ShipCore : MonoBehaviour {
     private Damagable damagable;
     private Connection[] connections;
 
+    private Renderer[] renderers;
+    private bool isWrappingX = false;
+    private bool isWrappingY = false;
+
     public int FactionId { get; set; }
     public Rigidbody2D Rb2D => rb2D;
 
@@ -30,11 +34,20 @@ public class ShipCore : MonoBehaviour {
         }
     }
 
-    private void Start() {
+    private void Start()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
         rb2D.AddForceAtPosition(Vector2.one * 15f, Vector2.left);
     }
 
     public void Update()
+    {
+        ScreenWrap();
+        CheckBaseThrusters();
+        
+    }
+
+    private void CheckBaseThrusters()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
@@ -63,6 +76,54 @@ public class ShipCore : MonoBehaviour {
         {
             ThrustDeactivated(RightThrustPosition);
         }
+    }
+
+    private bool CheckRenderers()
+    {
+        foreach(Renderer renderer in renderers)
+        {
+            if (renderer.isVisible)
+                return true;
+        }
+
+        return false;
+    }
+
+    private void ScreenWrap()
+    {
+        var isVisible = CheckRenderers();
+
+        if (isVisible)
+        {
+            isWrappingX = false;
+            isWrappingY = false;
+            return;
+        }
+
+        if (isWrappingX && isWrappingY)
+        {
+            return;
+        }
+
+        var cam = Camera.main;
+        var viewportPosition = cam.WorldToViewportPoint(transform.position);
+        var newPosition = transform.position;
+
+        if (!isWrappingX && (viewportPosition.x > 1 || viewportPosition.x < 0))
+        {
+            newPosition.x = -newPosition.x;
+
+            isWrappingX = true;
+        }
+
+        if (!isWrappingY && (viewportPosition.y > 1 || viewportPosition.y < 0))
+        {
+            newPosition.y = -newPosition.y;
+
+            isWrappingY = true;
+        }
+
+        transform.position = newPosition;
     }
 
     public void RegisterConnectable(Connectable connectable) {
